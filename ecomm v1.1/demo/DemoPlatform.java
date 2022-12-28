3b2d87ab834bd8bd15112931a1cfa8386688f45d
Achintya Harsha <achintyaharsha03@gmail.com>
2022-12-28 12:24:21 +0530

    started

---
+++ ecomm v1.1/demo/DemoPlatform.java
@@ -0,0 +1,20 @@
+package demo;
+
+import ecomm.Platform;
+import ecomm.Seller;
+
+public class DemoPlatform extends Platform {
+
+	@Override
+	public boolean addSeller(Seller aSeller) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void processRequests() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ ecomm v1.1/ecomm/Product.java
@@ -0,0 +1,13 @@
+package ecomm;
+
+public abstract class Product {
+
+	// common queries to get category, unique name, price, and number available
+	// public abstract Globals.Category getCategory();
+	public abstract String getName();
+	public abstract String getProductID();
+	public abstract float getPrice();
+	public abstract int getQuantity();
+}
+
+
