12e7e37fb18e2513feef05272cc13d94a2cec414
Achintya Harsha <achintyaharsha03@gmail.com>
2023-01-06 22:37:30 +0530

    final commit

--- ecomm v1.1/demo/ProductSorter.cpp
+++ ecomm v1.1/demo/ProductSorter.cpp
@@ -12,7 +12,7 @@ void ProductSorter::setParameter(string parameter)
 bool ProductSorter::operator()(vector<string> v1, vector<string> v2)
 {
 	if (parameter == "Price")	// To sort by product price
-		return v1[4] < v2[4];
+		return stof(v1[4]) < stof(v2[4]);
 
 	else if (parameter == "Name")	// To sort by product name
 		return v1[2] < v2[2];
